48ef14558a771842e3150ac6f2dbd8b35aae30bf
GoodGoodStudyDayDayUp <61325421+ijustwanthaveaname@users.noreply.github.com>
2020-11-12 21:03:44 +0800

    Update positive_selection1.sh

--- positive_selection/positive_selection1.sh
+++ positive_selection/positive_selection1.sh
@@ -3,11 +3,11 @@ ls *.fasta|while read id
 do
 #TransDecoder.LongOrfs -t ${id}
 #TransDecoder.Predict -t ${id}
-t_coffee -other_pg seq_reformat -in ${id} -action +translate -output fasta_seq > protein_${id}
+#t_coffee -other_pg seq_reformat -in ${id} -action +translate -output fasta_seq > protein_${id}
 mafft protein_${id} > aln_protein_${id}
 trimal -automated1 -in aln_protein_${id} -out protein_trim_${id}
 pal2nal.pl protein_trim_${id} ${id} -output paml > ${id%.fasta}.paml
-iqtree -s protein_trim_HBB.fasta -m MFP
+#iqtree -s protein_trim_HBB.fasta -m MFP
 #进行支位点模型
 done
 ls *.paml|while read id
